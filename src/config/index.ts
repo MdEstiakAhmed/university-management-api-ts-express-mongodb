4f364456bc22109cd12632899e2c3b212d1680d5
mdestiakahmed <estiak@tikweb.com>
2023-07-12 17:51:24 +0600

    initialize

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({
+    path: path.join(process.cwd(), process.env.NODE_ENV === 'production' ? '.env' : '.env.dev')
+});
+
+export default {
+    port: process.env.PORT,
+    databaseURL: process.env.DATABASE_URL,
+};
